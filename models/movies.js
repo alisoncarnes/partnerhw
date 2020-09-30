ad72d1c30d4fa131948f7538455897e21425d8c3
Alison Carnes <alisoncarnes@alisons-air.lan>
2020-09-30 10:06:20 -0700

    fixed merge conflic

--- models/movies.js
+++ models/movies.js
@@ -6,10 +6,9 @@ year: Number,
 img: { type: String, default: 'https://via.placeholder.com/200' }
 })
 
-<<<<<<< HEAD
+
 const Movies = mongoose.model('Movies', moviesSchema)
-=======
-const Movies = mongoose.model('Movies', birdSchema)
->>>>>>> ca9d902aab1950d83adc30f2a307347c7c6ab71a
+
+
 
 module.exports = Movies
