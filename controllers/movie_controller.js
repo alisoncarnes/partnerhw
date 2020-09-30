059b049703db7d133f810b2b8cde46f04f7aaa7f
Alison Carnes <alisoncarnes@alisons-air.lan>
2020-09-30 10:16:38 -0700

    pushing files

--- controllers/movie_controller.js
+++ controllers/movie_controller.js
@@ -4,4 +4,14 @@ const Movies = require('../models/movies.js')
 const movieSeed = require('../models/movies_seed.js')
 
 
+//POST
+
+movies.post('/', (req, res)=>{
+  Movies.create(req.body, (err, createdMovies)=>{
+    Movies.find({}, (err, foundMovies)=>{
+      res.json(foundMovies)
+    })
+  })
+})
+
 module.exports = movies
